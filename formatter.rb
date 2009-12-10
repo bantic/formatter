bce00a938dec162e1014862cc8051ecd5845627d
Cory <cory.forsyth@gmail.com>
2009-12-10 11:11:22 -0500

    initial commit

---
+++ formatter.rb
@@ -0,0 +1,32 @@
+class Formatter
+  class << self
+    def format(text)
+      @separator = separator(text)
+      raise "No separator found (should be =>, -> or =)" if @separator.nil?
+      
+      lines = text.split("\n")
+      longest_length = longest_length_before_separator(lines,@separator)
+      lines.collect do |line|
+        before_sep = match_before_separator(line, @separator)
+        sub_with = before_sep + " " * (longest_length - before_sep.length + 1)
+        line.sub(/#{before_sep}\s+/, sub_with)
+      end.join("\n") + "\n"
+    end
+    
+    def longest_length_before_separator(lines, separator)
+      lines.collect do |line|
+        match_before_separator(line, separator).length
+      end.sort.last
+    end
+    
+    def match_before_separator(line, separator)
+      line.match(/(.*)\s+#{separator}/)[1]
+    end
+    
+    def separator(text)
+      if match = text.match(/->|=>|=/)
+        match[0]
+      end
+    end
+  end
+end
---
+++ spec/formatter_spec.rb
@@ -0,0 +1,52 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Formatter do
+  before do
+    @text_blob_hr = <<-EOB
+      "first_one" => "def",
+      "second" => "jkl",
+      "third_and_longest_one" => "mno",
+    EOB
+    
+    @text_blob_scala_hr = <<-EOB
+      "first_one" -> "def",
+      "second" -> "jkl",
+      "third_and_longest_one" -> "mno",
+    EOB
+    
+    @text_blob_equals = <<-EOB
+      "first_one" = "def"
+      "second" = "jkl"
+      "third_and_longest_one" = "mno"
+    EOB
+  end
+  
+  it "should raise an error if no separator is found" do
+    lambda { Formatter.format("sdlkfjsldkfj asdfihsadfh lskdjflaksdf") }.should raise_error
+  end
+  
+  it "should find the longest length line before the separator" do
+    Formatter.longest_length_before_separator(["abc -> 3", "abcdef -> 4"], "->").should == "abcdef".length
+  end
+  
+  it "should find the match before the separator" do
+    Formatter.match_before_separator("abc -> 3", "->").should == "abc"
+  end
+  
+  it "should work" do
+    @expected = <<-EOB
+      "first_one"             => "def",
+      "second"                => "jkl",
+      "third_and_longest_one" => "mno",
+    EOB
+    Formatter.format(@text_blob_hr).should == @expected
+  end
+  
+  describe "separator" do
+    it "should find the separator" do
+      Formatter.separator(@text_blob_hr).should == "=>"
+      Formatter.separator(@text_blob_scala_hr).should == "->"
+      Formatter.separator(@text_blob_equals).should == "="
+    end
+  end
+end
